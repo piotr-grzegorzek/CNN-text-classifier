a6991183513958bba6d0de9726dd193c9f7755d8
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-06-17 20:29:10 +0200

    refactor: false_classified

--- false_classified.py
+++ false_classified.py
@@ -1,23 +1,35 @@
 import keras
 import numpy as np
-from run import x_train, x_test, y_test, maxlen
+from run import x_train, x_test, y_test, maxlen, imdb
 import utils
 
 x_train, x_test = utils.pad_sequences(x_train, x_test, maxlen)
 
-# Load the model
 model = keras.models.load_model("model.keras")
 
-# Make predictions
 predictions = model.predict(x_test)
 
-# Find the indices of the wrong predictions
+predictions = [type(y)(p) for p, y in zip(predictions.flatten(), y_test)]
+
+assert all(isinstance(p, type(y)) for p, y in zip(predictions, y_test))
+
 wrong_predictions = np.where(predictions != y_test)[0]
 
-# Print out the details of the wrong predictions
-for i in wrong_predictions:
-    index = wrong_predictions[i]
-    print(f"Recenzja numer {index}")
-    print(f"Rzeczywista etykieta: {y_test[index]}")
-    print(f"Przewidziana etykieta: {predictions[index][0]}")
+word_index = imdb.get_word_index()
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+
+def decode_review(text):
+    return " ".join([reverse_word_index.get(i, "?") for i in text])
+
+
+percentage_wrong = (len(wrong_predictions) / len(predictions)) * 100
+print(f"The percentage of wrong predictions is {percentage_wrong}%")
+
+for i in wrong_predictions[10:15]:
+    print(f"Review number {i}")
+    print(f"Actual label: {y_test[i]}")
+    print(f"Review text: {decode_review(x_test[i])}")
+    print(f"Predicted label: {predictions[i]}")
     print("\n")
