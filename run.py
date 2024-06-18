7040e2b1ba4be048dcf482c980c8c1c0bd082496
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-06-18 13:19:42 +0200

    refactor: static architecture based on auto tuner results

--- run.py
+++ run.py
@@ -11,11 +11,10 @@ from keras.layers import (  # type: ignore
 from keras.optimizers import Adam  # type: ignore
 from keras._tf_keras.keras.preprocessing.text import Tokenizer  # type: ignore
 from keras.preprocessing.sequence import pad_sequences  # type: ignore
-from keras.callbacks import EarlyStopping  # type: ignore
-from keras_tuner import HyperModel
-from keras_tuner.tuners import Hyperband
+from keras.callbacks import EarlyStopping, ModelCheckpoint  # type: ignore
 import matplotlib.pyplot as plt  # type: ignore
 from sklearn.model_selection import train_test_split  # type: ignore
+from keras.models import load_model  # type: ignore
 
 max_features = 20000
 max_len = 200
@@ -57,123 +56,74 @@ x_train, x_val, y_train, y_val = train_test_split(
 
 if __name__ == "__main__":
 
-    class AGNewsHyperModel(HyperModel):
-        def build(self, hp):
-            model = Sequential()
-            model.add(
-                Embedding(
-                    max_features,
-                    hp.Int(
-                        "embedding_output_dim", min_value=32, max_value=256, step=32
-                    ),
-                    input_length=max_len,
-                )
-            )
-            model.add(
-                SpatialDropout1D(
-                    hp.Float("spatial_dropout", min_value=0.1, max_value=0.5, step=0.1)
-                )
-            )
-            for i in range(hp.Int("num_conv_layers", 1, 3)):
-                model.add(
-                    Conv1D(
-                        filters=hp.Int(
-                            f"filters_{i}", min_value=32, max_value=256, step=32
-                        ),
-                        kernel_size=hp.Choice(f"kernel_size_{i}", [3, 5, 7]),
-                        activation="relu",
-                    )
-                )
-                model.add(
-                    Dropout(
-                        hp.Float(
-                            f"dropout_rate_conv_{i}",
-                            min_value=0.1,
-                            max_value=0.5,
-                            step=0.1,
-                        )
-                    )
-                )
-            model.add(GlobalMaxPooling1D())
-            model.add(
-                Dropout(
-                    hp.Float(
-                        "dropout_rate_pool", min_value=0.1, max_value=0.5, step=0.1
-                    )
-                )
-            )
-            model.add(
-                Dense(
-                    units=hp.Int("hidden_units", min_value=32, max_value=256, step=32),
-                    activation="relu",
-                )
-            )
-            model.add(
-                Dropout(
-                    hp.Float(
-                        "dropout_rate_dense", min_value=0.1, max_value=0.5, step=0.1
-                    )
-                )
-            )
-            model.add(Dense(units=4, activation="softmax"))
-            model.compile(
-                loss="sparse_categorical_crossentropy",
-                optimizer=Adam(
-                    learning_rate=hp.Float(
-                        "learning_rate",
-                        min_value=1e-4,
-                        max_value=1e-2,
-                        sampling="LOG",
-                        default=1e-3,
-                    )
-                ),
-                metrics=["accuracy"],
-            )
-            return model
+    model = Sequential()
+    model.add(
+        Embedding(
+            max_features,
+            output_dim=128,
+            input_length=max_len,
+        )
+    )
+    model.add(SpatialDropout1D(0.5))
 
-    hypermodel = AGNewsHyperModel()
-
-    tuner = Hyperband(
-        hypermodel,
-        objective="val_accuracy",
-        max_epochs=epochs,
-        hyperband_iterations=2,
-        directory="hyperband2",
-        project_name="ag_news",
+    model.add(
+        Conv1D(
+            filters=256,
+            kernel_size=5,
+            activation="relu",
+        )
+    )
+    model.add(Dropout(0.3))
+    model.add(GlobalMaxPooling1D())
+    model.add(
+        Dense(
+            units=32,
+            activation="relu",
+        )
+    )
+    model.add(Dropout(0.2))
+    model.add(Dense(units=4, activation="softmax"))
+    model.compile(
+        loss="sparse_categorical_crossentropy",
+        optimizer=Adam(
+            learning_rate=0.00049342,
+        ),
+        metrics=["accuracy"],
     )
 
     early_stopping = EarlyStopping(
         monitor="val_accuracy", patience=3, restore_best_weights=True
     )
 
-    tuner.search(
-        x_train,
-        y_train,
-        epochs=epochs,
-        validation_data=(x_val, y_val),
-        batch_size=batch_size,
-        callbacks=[early_stopping],
+    # Modify the ModelCheckpoint callback to save only the best model
+    model_checkpoint = ModelCheckpoint(
+        "best_model.keras",
+        monitor="val_accuracy",
+        verbose=1,
+        save_best_only=True,
+        mode="max",
     )
 
-    best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
-    print(f"Best hyperparameters: {best_hps.values}")
-
-    model = tuner.hypermodel.build(best_hps)
     history = model.fit(
         x_train,
         y_train,
         epochs=epochs,
         batch_size=batch_size,
         validation_data=(x_val, y_val),
-        callbacks=[early_stopping],
+        callbacks=[
+            early_stopping,
+            model_checkpoint,  # Use the modified ModelCheckpoint callback
+        ],
     )
 
-    score = model.evaluate(x_test_padded, y_test_data)
+    best_model = load_model("best_model.keras")
+
+    score = best_model.evaluate(x_test_padded, y_test_data)
     print("Test loss:", score[0])
     print("Test accuracy:", score[1])
 
-    model.save("model.keras")
-    print(model.summary())
+    # Print the summary of the best model
+    print(best_model.summary())
 
     # Plot training & validation accuracy values
     plt.figure(figsize=(12, 6))
